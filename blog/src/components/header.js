bc566ad3112440ce21b6f8f9c4b61612d1f6d2e9
Itzel-MM <montali1@gmatc.matc.edu>
2022-05-06 20:07:13 -0500

    Styled Components

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -1,9 +1,46 @@
-import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import styled from 'styled-components'
+
+//import * as React from "react"
+//import PropTypes from "prop-types"
+//import { Link } from "gatsby"
+
+const Outer = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Inner = styled.div`
+  margin: 0px auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const H1 = styled.h1`
+  margin: 0px;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    color: purple;
+  }
+`
 
 const Header = ({ siteTitle }) => (
-  <header
+  <Outer>
+    <Inner>
+      <H1>
+        <StyledLink to="/">
+          {siteTitle}
+        </StyledLink>
+      </H1>
+    </Inner>
+  </Outer>
+  /*<header
     style={{
       background: `rebeccapurple`,
       marginBottom: `1.45rem`,
@@ -28,7 +65,7 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
-  </header>
+  </header>*/
 )
 
 Header.propTypes = {
